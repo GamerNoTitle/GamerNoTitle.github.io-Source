58f34639155c9cc5d9807c461ada98e1d2277fb9
GamerNoTitle <admin@bili33.top>
2021-08-02 21:56:06 +0800

    No.2 Commit in Third Week Backup

--- deploy-without-updating-data.bat
+++ deploy-without-updating-data.bat
@@ -1,3 +1,3 @@
 @echo off
 Set /p times=<times.txt 
-hexo cl && hexo d -g && hexo cl && git add . && git commit -m "No.%times% Commit in Second Week Backup" && git push && start change-times.bat
+hexo cl && hexo d -g && hexo cl && git add . && git commit -m "No.%times% Commit in Third Week Backup" && git push && start change-times.bat
