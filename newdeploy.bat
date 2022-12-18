1d4427044e0b970c1f8616ae1a8dda9df14e11c8
GamerNoTitle <GamerNoTitle@outlook.com>
2022-12-19 07:30:07 +0800

    No.510  Commit Backup

--- newdeploy.bat
+++ newdeploy.bat
@@ -1,3 +1,3 @@
 @echo off
 Set /p times=<times.txt 
-git add . && git commit -m "No.%times% Commit Backup" && git push && start change-times.bat
+git pull & git add . && git commit -m "No.%times% Commit Backup" && git push && start change-times.bat
