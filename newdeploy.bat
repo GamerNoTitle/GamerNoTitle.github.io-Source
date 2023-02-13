ab5c08f56c5c254072f25cbb8b49d9dfe14115df
GamerNoTitle <GamerNoTitle@outlook.com>
2023-02-13 17:39:05 +0800

    17:39:05.72 2023/02/13 周一 Commit

--- newdeploy.bat
+++ newdeploy.bat
@@ -3,11 +3,11 @@
 title "Setting date & time"
 Set date = date /t
 Set time = time /t
-title "Pull new commits"
+title Pull new commits
 git pull
-title "Cleaning cache"
+title Cleaning cache
 call hexo cl
-title "Pushing new changes"
+title Pushing new changes
 git add . 
 git commit -m "%time% %date% Commit"
 git push
